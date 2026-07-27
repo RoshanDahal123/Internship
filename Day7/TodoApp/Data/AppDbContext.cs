30ea3fd8ba4371f416ccceea181be7178fb30028
RoshanDahal123 <dahalroshan765@gmail.com>
2026-07-27 15:01:44 +0545

    feat(Data):add AppDbContext

--- Day7/TodoApp/Data/AppDbContext.cs
+++ Day7/TodoApp/Data/AppDbContext.cs
@@ -1,8 +1,21 @@
-﻿namespace TodoApp.Data
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApp.Data;
 
 
+
+public class AppDbContext:DbContext 
 {
-    public class AppDbContext
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public DbSet<TodoItem> Todos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<TodoItem>().HasData(
+            new TodoItem { Id = 1, Title = "Learn ASP.NET Core", Description = "Master the framework", Priority = 3 },
+             new TodoItem { Id = 2, Title = "Build a Todo App", Description = "Create a complete project", Priority = 2 },
+             new TodoItem { Id = 3, Title = "Write Documentation", Description = "Document the code", IsCompleted = true, CompletedAt = DateTime.Now.AddDays(-1), Priority = 1 }
+            );
     }
+
 }
