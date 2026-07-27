7415fbd82107e108200e35d35c580df1c111bf52
RoshanDahal123 <dahalroshan765@gmail.com>
2026-07-27 14:51:13 +0545

    Add todoviewModel.cs

--- Day7/TodoApp/Models/TodoViewModel.cs
+++ Day7/TodoApp/Models/TodoViewModel.cs
@@ -1,6 +1,8 @@
-﻿namespace TodoApp.Models
+﻿namespace TodoApp.Models;
+public class TodoViewModel
 {
-    public class TodoViewModel
-    {
-    }
+    public List<TodoItem> Todos { get; set; } = new();
+    public int CompletedCount => Todos?.Count(t => t.IsCompleted) ?? 0;
+    public int TotalCount => Todos?.Count ?? 0;
+    public double CompletionRate => TotalCount > 0 ? (double)CompletedCount / TotalCount * 100 : 0;
 }
