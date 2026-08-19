8c85b31df8b82dcc33a14164c41af569b1f1cc63
RoshanDahal123 <dahalroshan765@gmail.com>
2026-08-19 15:03:15 +0545

    learned about the communication between the view and the controller how it connects and how the data can be passed

---
+++ Day23/AspDotnetCore/FirstEmpty/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstEmpty.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+           ViewBag.Title = "Home";
+            return View();
+        }
+        public IActionResult Name()
+        {
+            ViewBag.Title = "Home";
+            return View();
+        }
+    }
+}
--- Day23/AspDotnetCore/FirstEmpty/Program.cs
+++ Day23/AspDotnetCore/FirstEmpty/Program.cs
@@ -1,16 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//registering the services
+
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
-app.Use((context,next) =>
-{
-    context.Response.WriteAsync("Text from the middleware1");
-    return next();
-});
-app.Use((context, next) =>
-{
-    context.Response.WriteAsync("Text from the middleware2");
-    return next();
-});
-app.Run((context) => context.Response.WriteAsync("Response from Middleware Run"));
-app.MapGet("/", () => "Hello World!");
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//app.MapGet("/", () => "Hello World!");
 
 app.Run();
